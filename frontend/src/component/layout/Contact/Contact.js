93edd6f2209fbaba5e755b8d709aa0b0861f67c4
ashishmohan0522 <54908847+ashishmohan0522@users.noreply.github.com>
2022-09-08 01:45:32 +0530

    Update Contact.js

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -5,7 +5,7 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
+      <a className="mailBtn" href="mailto:dude01the@gmail.com">
         <Button>Contact: dude01the@gmail.com</Button>
       </a>
     </div>
